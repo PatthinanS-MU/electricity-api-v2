7384b233ddf8c005035681bd6d27d442b696c083
PatthinanS-MU <patthinan.suk@student.mahidol.edu>
2026-03-23 15:14:40 +0700

    docs: added unit tests and quality gate to pipeline

--- tests/api.test.js
+++ tests/api.test.js
@@ -9,5 +9,17 @@ describe("Electricity API Endpoints", () => {
   });
 
   // Test Case 2: Specific Province Usage
+  it("should return electricity usage for a specific province and year", async () => {
+    const response = await request(app).get("/api/usage/Bangkok/2020");
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBeInstanceOf('Data not found');
+  });
+
   // Test Case 3: Verify Data Structure for Users
+   it("should return total electricity users by year", async () => {
+    const response = await request(app).get("/api/users/Bangkok/2020");
+    expect(response.status).toEqual(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
 });
